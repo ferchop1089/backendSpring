013f14a19b0bcdeb7f9118c786eb1a405f3274a5
Eduard Carvajal <efcarvajal10@gmail.com>
2019-01-07 13:10:21 -0500

    Primer commit backend Spring

---
+++ src/main/java/com/tutorial/spring/backend/BackendSpringApplication.java
@@ -0,0 +1,14 @@
+package com.tutorial.spring.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tutorial/spring/backend/BackendSpringApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tutorial.spring.backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
